52b691dfeb160a843370d8623371377ff6e552a7
xygodcyx <1323943635@qq.com>
2025-03-19 12:16:35 +0800

    全部完成！流式传输，硅基流动的api

--- server-stream.js
+++ server-stream.js
@@ -6,28 +6,33 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
-const DEEPSEEK_API_KEY = 'sk-5199daec3306456790df5c02f9e43a5c';
+// const DEEPSEEK_API_KEY = 'sk-5199daec3306456790df5c02f9e43a5c';
+const DEEPSEEK_API_KEY = 'sk-bbmnohyyguoxyswllncahbixtfjjsdijixwxhuwzkmigddrj';
 
 const openai = new OpenAI({
-  baseURL: 'https://api.deepseek.com',
+  baseURL: 'https://api.siliconflow.cn/v1',
   apiKey: DEEPSEEK_API_KEY,
 });
 
-app.get('/new-question', async (req, res) => {
+app.post('/new-question', async (req, res) => {
   try {
+    const { oldQuestion } = req.body;
+    console.log('oldQuestion:', oldQuestion);
+
     const stream = await openai.chat.completions.create({
       messages: [
         {
           role: 'system',
-          content: `你是一个专业出题老师（随机因子：${Math.random()}），
-请生成小学难度的问题，领域：数学、科学、历史、文学。只返回问题本身。`,
+          content: `这是旧问题：${oldQuestion}，一定不要重复之前输出过的问题，领域也尽量不要重复，你是一个专业出题老师，
+请生成小学难度的问题，领域：数学、科学、历史、文学、金融、计算机，只选择其中一个，不要太难，也不要太简单，只返回问题本身即可。`,
         },
       ],
       stream: true, // 启用流式输出
       caches: false,
-      temperature: 2.0,
-      model: 'deepseek-chat',
+      temperature: 0.2,
+      model: 'deepseek-ai/DeepSeek-V2.5',
     });
 
     res.setHeader('Content-Type', 'text/plain');
@@ -51,7 +56,7 @@ app.post('/check-answer', async (req, res) => {
       messages: [
         {
           role: 'system',
-          content: `请用以下格式验证答案：包含：1.判断 2.正确答案 3.知识点解释（200字内）并在回答的末尾给出得分，分数在0-100之间，正确性在0-100之间,并用以下格式回复(末尾不要有其他字符)，{"score":number,"correct":number}原始问题：${question}`,
+          content: `请用以下格式验证答案，使用html标签输出：包含：1.判断正确 2.给出正确答案 3.给出知识点解释（200字内）并在回答的末尾给出得分，分数在0-100之间，正确性在0-100之间,只在末尾输出json，在末尾不要有\`\`json标签\`\`只在末尾输出纯洁的json：{"score":number,"correct":number}，原始问题：${question}`,
         },
         {
           role: 'user',
@@ -60,8 +65,8 @@ app.post('/check-answer', async (req, res) => {
       ],
       stream: true,
       caches: false,
-      temperature: 2.0,
-      model: 'deepseek-chat',
+      temperature: 0.2,
+      model: 'deepseek-ai/DeepSeek-V2.5',
     });
 
     res.setHeader('Content-Type', 'text/plain');
@@ -104,8 +109,8 @@ async function validateAnswerWithRetry(question, answer) {
       },
     ],
     caches: false,
-    temperature: 2.0,
-    model: 'deepseek-chat',
+    temperature: 0.2,
+    model: 'deepseek-ai/DeepSeek-V2.5',
   });
   res.setHeader('Response-Mode', 'stream');
   return JSON.parse(response.choices[0].message.content.match(/{[\s\S]*?}/)[0]);
